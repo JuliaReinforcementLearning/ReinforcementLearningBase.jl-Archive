b091df42881c64488e1ed6b063811a8cba076a20
Jun Tian <tianjun.cpp@gmail.com>
2020-07-29 19:02:56 +0800

    add RandomStartPolicy

--- src/implementations/policies.jl
+++ src/implementations/policies.jl
@@ -1,4 +1,4 @@
-export RandomPolicy
+export RandomPolicy, RandomStartPolicy
 
 using Random
 
@@ -59,5 +59,36 @@ function get_prob(p::RandomPolicy{Nothing}, env, a)
         1.0 / length(legal_actions)
     else
         0.0
+    end
+end
+
+#####
+# RandomStartPolicy
+#####
+
+Base.@kwdef mutable struct RandomStartPolicy{P,R<:RandomPolicy} <: AbstractPolicy
+    policy::P
+    random_policy::R
+    num_rand_start::Int
+end
+
+function (p::RandomStartPolicy)(env)
+    p.num_rand_start -= 1
+    if p.num_rand_start < 0
+        p.policy(env)
+    else
+        p.random_policy(env)
+    end
+end
+
+update!(p::RandomStartPolicy, experience) = update!(p.policy, experience)
+
+for f in (:get_prob, :get_priority)
+    @eval function $f(p::RandomStartPolicy, args...)
+        if p.num_rand_start < 0
+            $f(p.policy, args...)
+        else
+            $f(p.random_policy, args...)
+        end
     end
 end
